be1f24d618ed983a3b0c4a764fd7fb8d9ef1b778
Tom Chap <tom@wooclap.com>
2020-06-01 21:30:20 +0200

    clean build script: do not copy src file in build folder
    only copy the compiled files

--- love-sublime-build.bat
+++ love-sublime-build.bat
@@ -4,11 +4,12 @@ mkdir gen_love
 mkdir gen_love\lib
 mkdir gen_love\res
 
-copy src\*.moon gen_love
 copy res\* gen_love\res
 copy lib\* gen_love\lib
 
-moonc gen_love
+cd src
+moonc -t ../gen_love *.moon
+cd ..
 
 cd gen_love
 
